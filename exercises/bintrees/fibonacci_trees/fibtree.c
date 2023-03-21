07ff15202576e1ed5cbeb07526948e3a0a760f62
Nicolas Flasque <nicolas.flasque@gmail.com>
2023-03-21 16:19:08 +0100

    trees - bintrees - char bintrees

---
+++ exercises/bintrees/fibonacci_trees/fibtree.c
@@ -0,0 +1,35 @@
+//
+// Created by flasque on 11/10/2022.
+//
+t_tree fibtree(int n)
+{
+    t_tree t;
+    p_node father, pcur, pzero, pone;
+
+    pzero = createNode(0);
+    pone = createNode(1);
+    pone->left = pone->right = pzero;
+    pcur = pone;
+    if (n==0)
+    {
+        t.root = pzero;
+    }
+    else if (n==1)
+        {
+            t.root = pone;
+        }
+        else
+        {
+            for (int i = 2; i <= n; i++)
+            {
+                father = createNode(i);
+                father->left = pcur;
+                father->right = pcur->left;
+                pcur = father;
+            }
+
+            t.root = father;
+        }
+    return t;
+
+}
